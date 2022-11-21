88e953f13e9047eb7e8ac9d7a55503e4690f5822
Amy Li <amyl@buffalo.edu>
2022-11-21 13:53:45 -0500

    commit tree c51cd7e1db1fdc6c712d545d1e17b4a3046bc298

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()>0);
+const num4 = 32;
+const num3 = 8;
+const mod = (num4 / num3);
+const max = Math.max (5, 6, 7, 8, 9, 20)
